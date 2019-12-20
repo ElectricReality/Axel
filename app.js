a542854ca3377e80429966b08f1a3dcc6c9500e1
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-12-20 22:31:25 +0800

    test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
-app.set('views', __dirname + '/public');
+app.set('views', __dirname + '/public/');
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Axel is listening on ' + port));
 
