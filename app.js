d8a586536dc6f026f59b9faaa56765b1db6e4d1b
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-12-28 23:49:33 +0800

    yes

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.get("/", function(req, res, next) {
 
 app.get("/register", async(req,res,next) => {
   let user = await mongo.getall('user')
-  if(user == []) {
+  if(array.length == 0) {
     return res.render("register.ejs", { message: 'Please register your new credentials!' });
   }
   res.redirect("/login")
@@ -60,7 +60,7 @@ app.get("/register", async(req,res,next) => {
 
 app.post("/register", async(req,res,next) => {
   let user = await mongo.getall('user')
-  if(user == []) {
+  if(array.length == 0) {
     bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
       if(err) {
         console.log(err)
