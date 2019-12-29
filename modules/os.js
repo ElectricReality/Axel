1382a34b26c0edee9189adff5b210e78c169191a
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-12-29 23:13:12 +0800

    await

--- modules/os.js
+++ modules/os.js
@@ -2,22 +2,22 @@ const os = require('os-utils')
 
 module.exports = {
   platform: async () => {
-    return os.platform();
+    return await os.platform();
   },
 
   cpuUsage: async () => {
     os.cpuUsage(function(v){
-	     return v
+	     return await v
     });
   },
 
   usedMem: async () => {
     let used = os.totalmem() - os.freemem()
-    return used
+    return await used
   },
 
   sysUp: async () => {
-    return os.sysUptime()
+    return await os.sysUptime()
   }
 
 }
