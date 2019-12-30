0dc4e18ce88364be0802b823dfed0045bf7eeb06
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-12-31 00:36:12 +0800

    test

--- modules/nginx.js
+++ modules/nginx.js
@@ -56,7 +56,7 @@ module.exports = {
 
     docker.listServices({}).then(async function(ser) {
       let result = await ser.find(s => s.Spec.Name == "axel-system-nginx")
-      if(!result){
+      if (!result) {
         console.log("Creating Service")
         docker.createService(options, function(err, csdata) {
           if (err) {
@@ -66,39 +66,39 @@ module.exports = {
         })
       } else {
         let opts = {
-        "Name": "axel-system-nginx",
-        "version": parseInt(result.Version.Index),
-        "TaskTemplate": {
-          "ContainerSpec": {
-            "Image": "axel-nginx"
+          "Name": "axel-system-nginx",
+          "version": parseInt(result.Version.Index),
+          "TaskTemplate": {
+            "ContainerSpec": {
+              "Image": "axel-nginx"
+            },
+            "Resources": {
+              "Limits": {},
+              "Reservations": {}
+            },
+            "RestartPolicy": {},
+            "Placement": {}
           },
-          "Resources": {
-            "Limits": {},
-            "Reservations": {}
+          "Mode": {
+            "Replicated": {
+              "Replicas": 1
+            }
           },
-          "RestartPolicy": {},
-          "Placement": {}
-        },
-        "Mode": {
-          "Replicated": {
-            "Replicas": 1
+          "Networks": [{
+            'Target': "axel-net",
+          }],
+          "UpdateConfig": {
+            "Parallelism": 1
+          },
+          "EndpointSpec": {
+            "ExposedPorts": [{
+              "Protocol": "tcp",
+              "Port": 80
+            }]
           }
-        },
-        "Networks": [{
-          'Target': "axel-net",
-        }],
-        "UpdateConfig": {
-          "Parallelism": 1
-        },
-        "EndpointSpec": {
-          "ExposedPorts": [{
-            "Protocol": "tcp",
-            "Port": 80
-          }]
-        }
-      };
+        };
         const service = docker.getService(result)
-        service.update(opts.stringify, function(err, sudata) {
+        service.update(opts.stringify(), function(err, sudata) {
           if (err) {
             return console.log(err)
           }
