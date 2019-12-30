256ff993cf7b0c9d00d3460ff5084ee144c8b49d
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-12-31 00:38:09 +0800

    tse

--- modules/nginx.js
+++ modules/nginx.js
@@ -98,7 +98,7 @@ module.exports = {
           }
         };
         const service = docker.getService(result)
-        service.update(opts.stringify(), function(err, sudata) {
+        service.update(SON.stringify(opts), function(err, sudata) {
           if (err) {
             return console.log(err)
           }
