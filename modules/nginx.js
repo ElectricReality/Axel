9fe5dddf5ea2f8a024a19f85814974bf3d8fb73c
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-12-30 23:32:12 +0800

    test

--- modules/nginx.js
+++ modules/nginx.js
@@ -12,7 +12,7 @@ module.exports = {
     //}
     docker.buildImage({
       context: './nginx',
-      src: ['Dockerfilenginx']
+      src: ['Dockerfile']
     }, {
       t: 'axel-nginx'
     }, function(error, output) {
