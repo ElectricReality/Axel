d67192c14d9dcca1e273f45900de37ec7e5bdfe3
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-12-30 22:36:17 +0800

    test

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ docker service create \
   --mount type=volume,source=axel-system-database-config,target=/data/configdb \
   mongo:latest > /dev/null 2>&1
 echo "Building Axel Image"
-docker build -f Dockerfile -t axel . > /dev/null 2>&1
+docker build -t axel . > /dev/null 2>&1
 echo "Starting Axel Service"
 docker service create \
   --name axel-system \
