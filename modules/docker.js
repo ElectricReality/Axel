c5d61a6ab71ee695ad89ffedfb6e85c063f50f57
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2020-01-02 01:34:53 +0800

    test

--- modules/docker.js
+++ modules/docker.js
@@ -5,68 +5,93 @@ const docker = new Docker({
 const tarfs = require('tar-fs');
 const path = require('path');
 const git = require('simple-git/promise')
+const fs = require("fs"); // Or `import fs from "fs";` with ESM
+
 
 module.exports = {
   axel: async () => {
-      let repo = 'https://github.com/ElectricReality/Axel.git';
-      await git().silent(true)
-        .clone(repo)
-        .then(() => console.log('Clone finish'))
-        .catch((err) => console.error('failed: ', err));
-      const filePath = path.join(process.cwd(), '/Axel');
-      const pack = await tarfs.pack(filePath);
-      let stream = await docker.buildImage(pack, {t: 'axel:latest'});
-      await new Promise((resolve, reject) => {
-        docker.modem.followProgress(stream, (err, res) => err ? reject(err) : resolve(res));
-      });
-      docker.listServices({}).then(async function(ser) {
-        let result = await ser.find(s => s.Spec.Name == "axel-system")
-        const service = docker.getService(result.ID)
-        let opts = {
-          "Name": "axel-system",
-          "version": parseInt(result.Version.Index),
-          "TaskTemplate": {
-            "ContainerSpec": {
-              "Image": "axel:latest",
-              "Mounts": [{
-                "Type": "bind",
-                "Source": "/var/run/docker.sock",
-                "Target": "/var/run/docker.sock"
-              }]
-            },
-            "Resources": {
-              "Limits": {},
-              "Reservations": {}
-            },
-            "RestartPolicy": {},
-            "Placement": {}
-          },
-          "Mode": {
-            "Replicated": {
-              "Replicas": 1
-            }
-          },
-          "Networks": [{
-            'Target': "axel-net",
-          }],
-          "UpdateConfig": {
-            "Parallelism": 1
-          },
-          "EndpointSpec": {
-            "Ports": [{
-              "Protocol": "tcp",
-              "TargetPort": 3000,
-              "PublishedPort": 3000,
-            }]
+    let repo = 'https://github.com/ElectricReality/Axel.git';
+    const filePath = path.join(process.cwd(), '/Axel');
+    if (fs.existsSync(filePath)) {
+      var rmdir = function(dir) {
+        var list = fs.readdirSync(dir);
+        for (var i = 0; i < list.length; i++) {
+          var filename = path.join(dir, list[i]);
+          var stat = fs.statSync(filename);
+
+          if (filename == "." || filename == "..") {
+            // pass these files
+          } else if (stat.isDirectory()) {
+            // rmdir recursively
+            rmdir(filename);
+          } else {
+            // rm fiilename
+            fs.unlinkSync(filename);
           }
-        };
-        service.update(opts, function(err, sudata) {
-          if (err) {
-            return console.log(err)
-          }
-          console.log(sudata)
-          return console.log("Axel Service Updated");
-        })
-      });
+        }
+        fs.rmdirSync(dir);
+      };
+      rmdir(filePath);
     }
+    await git().silent(true)
+      .clone(repo)
+      .then(() => console.log('Clone finish'))
+      .catch((err) => console.error('failed: ', err));
+    const pack = await tarfs.pack(filePath);
+    let stream = await docker.buildImage(pack, {
+      t: 'axel:latest'
+    });
+    await new Promise((resolve, reject) => {
+      docker.modem.followProgress(stream, (err, res) => err ? reject(err) : resolve(res));
+    });
+    docker.listServices({}).then(async function(ser) {
+      let result = await ser.find(s => s.Spec.Name == "axel-system")
+      const service = docker.getService(result.ID)
+      let opts = {
+        "Name": "axel-system",
+        "version": parseInt(result.Version.Index),
+        "TaskTemplate": {
+          "ContainerSpec": {
+            "Image": "axel:latest",
+            "Mounts": [{
+              "Type": "bind",
+              "Source": "/var/run/docker.sock",
+              "Target": "/var/run/docker.sock"
+            }]
+          },
+          "Resources": {
+            "Limits": {},
+            "Reservations": {}
+          },
+          "RestartPolicy": {},
+          "Placement": {}
+        },
+        "Mode": {
+          "Replicated": {
+            "Replicas": 1
+          }
+        },
+        "Networks": [{
+          'Target': "axel-net",
+        }],
+        "UpdateConfig": {
+          "Parallelism": 1
+        },
+        "EndpointSpec": {
+          "Ports": [{
+            "Protocol": "tcp",
+            "TargetPort": 3000,
+            "PublishedPort": 3000,
+          }]
+        }
+      };
+      service.update(opts, function(err, sudata) {
+        if (err) {
+          return console.log(err)
+        }
+        console.log(sudata)
+        return console.log("Axel Service Updated");
+      })
+    });
+  }
 }
