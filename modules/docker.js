63206f6cc8ef2c1c77f5936c23dd9159a00b7125
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2020-01-02 01:02:55 +0800

    test

--- modules/docker.js
+++ modules/docker.js
@@ -13,7 +13,7 @@ module.exports = {
         .clone(repo)
         .then(() => console.log('Clone finish'))
         .catch((err) => console.error('failed: ', err));
-      const filePath = '../Axel'
+      const filePath = path.join(process.cwd(), '/Axel');
       const pack = await tarfs.pack(filePath);
       docker.buildImage(pack, {t: 'axel'}, function (err, response){
         if(err) {
