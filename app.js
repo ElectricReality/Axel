a704d3e82178af0de6992c4807b387828d1ae9d5
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2020-01-18 15:49:17 +0800

    est

--- app.js
+++ app.js
@@ -225,5 +225,5 @@ app.get("/settings/update", async (req, res, next) => {
 });
 
 // Update Nginx settings
-console.log("Version 1.0.234thnhtrdfghj")
+console.log("Version 1.0.3erfghn")
 nginx.update()
