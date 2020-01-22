cf0fb48e2564a9eb86244e6057d5968e8ee4b3ed
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2020-01-22 20:47:33 +0800

    test

--- install.sh
+++ install.sh
@@ -11,28 +11,28 @@ echo "||         Starting Axel Installation         ||"
 echo "||                                            ||"
 echo "================================================"
 echo ""
-ipv4="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+ipv4="$(dig +short myip.opendns.com @resolver1.opendns.com)" > /dev/null 2>&1
 session="$(date +%s | sha256sum | base64 | head -c 32 )"
 echo "Starting Swarm"
-docker swarm init --advertise-addr ${ipv4}
+docker swarm init --advertise-addr ${ipv4} > /dev/null 2>&1
 echo "Starting Network"
-docker network create --driver overlay axel-net
+docker network create --driver overlay axel-net> /dev/null 2>&1
 echo "Starting Axel Database"
 docker service create \
   --name axel-system-database \
   --network axel-net \
   --mount type=volume,source=axel-system-database-data,target=/data/db \
   --mount type=volume,source=axel-system-database-config,target=/data/configdb \
-  mongo:latest
+  mongo:latest > /dev/null 2>&1
 echo "Building Axel Image"
-docker build -t axel:latest .
+docker build -t axel:latest . > /dev/null 2>&1
 echo "Starting Axel Service"
 docker service create \
   --name axel-system \
   --network axel-net \
-  --publish 8080:8080 \
+  --publish 8080:8080/tcp \
   --env session=${session} \
   --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
-  axel:latest
+  axel > /dev/null 2>&1
 echo " "
 echo "You can now start using Axel at http://${ipv4}:8080/login. Please set new username and password immediately at the admin dashboard!"
